e9818ab412bab1b68277d322ed4c664ac1d1ad12
bohdan_petchenko <pechka24@gmail.com>
2020-04-17 00:53:59 +0300

    done

--- js/Petchenko_HW08/script.js
+++ js/Petchenko_HW08/script.js
@@ -1,102 +1,127 @@
-/***** sort *****/
+/***** makeProfileTimer *****/
 
-var persons = [
-    {name: "Иван", age: 17},
-    {name: "Мария", age: 35},
-    {name: "Алексей", age: 73},
-    {name: "Яков", age: 12},
-]
+function makeProfileTimer(){
 
-function sort (objName, objKey, isReverse){ 
-    
-    objName.sort((a,b) => a[objKey] > b[objKey] ? 1 : -1);
+    let firstTime = performance.now()
 
-    if(isReverse === undefined || isReverse === true){
-        return objName
-    } else{
-       return objName.reverse()
+    var dataSecondTimer = function() {
+        let secondTime = performance.now();
+        let result = secondTime - firstTime;
+
+        return result
     }
-    
+    return dataSecondTimer
 }
-console.log(sort(persons, "age"))
 
 
-/***** array map *****/
+function doSomething(){
+    var somethingBad = Math.random() > 0.5;
+    if (somethingBad){
+        console.log("Something bad happens");
+        return;
+    }
+    console.log("All OK!");
+}
 
-var arr = ["1", {}, null, undefined, "500", 700]
-console.log(arr.map(x => (isNaN(+x) || x === null) ? x : +x))
+
+var timer = makeProfileTimer()
+doSomething();  
+console.log(timer() + ' milliseconds'); 
 
 
-/***** array reduce *****/
+/***** makeSaver *****/
 
-var arr2 = ["0", 5, 3, "string", null]
+function makeSaver(someFunc){
 
-function findNumber(someArr){
-    var arrNum = []
-
-    for(let param of someArr){
-
-        if(typeof(param) === 'number'){
-            arrNum.push(param) 
+    let isAlreadyRun;
+    let save;
+    
+    return function hold (){
+        if(!isAlreadyRun){
+            save = someFunc();
+            isAlreadyRun = true;
         }
+        return save;
     }
-    return arrNum
 }
-console.log(findNumber(arr2).reduce((x,y) => x*y))
+
+var saver = makeSaver(Math.random) 
+
+var value1 = saver()              
+var value2 = saver()              
+
+console.log(value1 === value2)            
+
+var saver2 = makeSaver(() => console.log('saved function called') || [null, undefined, false, '', 0, Math.random()][Math.ceil(Math.random()*6)])
+var value3 = saver2()
+var value4 = saver2()
+
+console.log(value3 === value4) 
+
+/***** Final Countdown *****/
+
+function countdown(countdown){
+    let value = countdown;
+    
+    setTimeout(function go() {
+        console.log(value);
+
+        if (value > 1){
+            setTimeout(go, 1000);
+        } else {console.log('Поехали!')}
+        value--;
+    }, 1000);
+    
+}
+countdown(5)
 
 
-/***** object filter *****/
+/***** myBind *****/
 
-var phone = {
-    brand: "meizu",
-    model: "m2",
-    ram: 2,
-    color: "black",
+var myBind = function(fn, context, someArr) {
+     
+    return function(...params){
+        
+        let filt = arrFiltered();
+
+        function arrFiltered(){
+            for(let itemSomeArr of someArr){
+                for(let i=0; i < [...params].length; i++){
+                    if(itemSomeArr == undefined){
+                        someArr.splice(someArr.indexOf(itemSomeArr),1, [...params][i])
+                    
+                    }
+                }
+            }
+            return someArr
+        };
+        // arrFiltered.unshift(...params)
+        console.log(fn.apply(context,[...filt]))
+        return fn.apply(context,[...filt])
+    }
+   
+    
 };
 
 
-function filter(someObj, key, value){
-            
-    for(let objKey in someObj){
+var pow5 = myBind(Math.pow, Math, [undefined, 5]) // первый параметр - функция для биндинга значений по умолчанию, 
+                                                  // второй - this для этой функции, третий - массив, в котором undefined означает
+                                                  // параметры, которые должны передаваться при вызове,
+                                                  // а другие значения являются значениями по умолчанию:
+var cube = myBind(Math.pow, Math, [undefined, 3]) // cube возводит число в куб
 
-        if(objKey !== key && value !== someObj[objKey]){
-            delete someObj[objKey]
-        }
-    } 
-    return someObj  
-}
+pow5(2) // => 32, вызывает Math.pow(2,5), соотнесите с [undefined, 5]
+cube(3) // => 27
 
-console.log(filter(phone, "color", 2));
 
-    
-/***** object map *****/
-var man = {
-    name: "Иван",
-    age: 17
-}
-
-var result = {};
-
-function myMap(objToMap){
-    
-    for(let [key, value] of Object.entries(objToMap)){
-        result[key+"_"] = value + "$";
-    }
-    return result;
-}
-
-console.log(myMap(man))
-
-/***** sum *****/
-
-function sum(value){
-    return (value === 1) ? 1 : value + sum(value - 1);
-}
-
-console.log(sum(5))
+var chessMin = myBind(Math.min, Math, [undefined, 4, undefined, 5,undefined, 8,undefined, 9])
+chessMin(-1,-5,3,15) // вызывает Math.min(-1, 4, -5, 5, 3, 8, 15, 9), результат -5
 
 
 
+var zeroPrompt = myBind(prompt, window, [undefined, "0"]) // аналогично, только теперь задается "0" как текст по умолчанию в prompt, 
+                                                          // а текст приглашения пользователя задается при вызове zeroPrompt
+var someNumber = zeroPrompt("Введите число")              // вызывает prompt("Введите число","0")
 
 
 
